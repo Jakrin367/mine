4cce70a32c0153171af644865c81bcc1e3d37d11
Jakein367 <nxydek28@gmail.com>
2024-12-11 11:25:46 +0700

    frist commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './compsnents/Navbar';
+import Carousel from './pages/Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
